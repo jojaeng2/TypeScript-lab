1d6fd1326d0bf74c760fcbf8ec796f2669a2cfbb
jojaeng2 <ds4ouj@naver.com>
2023-03-12 13:32:58 +0900

    feat : Function/Overloads

---
+++ Docs/parameter/Overloads.ts
@@ -0,0 +1,20 @@
+let suits = ["hearts", "spades", "clubs", "diamonds"]
+
+function pickCard(x : any) : any {
+    // 인자가 배열 또는 객체인지 확인
+    if (typeof x == "object") {
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+    else if (typeof x == "number") {
+        let pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
+
+let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+let pickedCard1 = myDeck[pickCard(myDeck)];
+console.log("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+
+let pickedCard2 = pickCard(15);
+console.log("card: " + pickedCard2.card + " of " + pickedCard2.suit);
